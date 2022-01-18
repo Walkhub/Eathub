6a42c5c7138f8db77ecf0056d4b0c45f16933afd
jhhong0509 <jhhong0509@naver.com>
2022-01-18 19:18:06 +0900

    FIX: Kotlin에서 SocketIO Jackson 동작하도록 수정

--- src/main/kotlin/com/eathub/eathub/global/socket/SocketIOConfig.kt
+++ src/main/kotlin/com/eathub/eathub/global/socket/SocketIOConfig.kt
@@ -4,8 +4,10 @@ import com.corundumstudio.socketio.Configuration
 import com.corundumstudio.socketio.SocketConfig
 import com.corundumstudio.socketio.SocketIOServer
 import com.corundumstudio.socketio.annotation.SpringAnnotationScanner
+import com.corundumstudio.socketio.protocol.JacksonJsonSupport
 import com.eathub.eathub.global.socket.error.SocketErrorController
 import com.eathub.eathub.global.socket.property.SocketProperties
+import com.fasterxml.jackson.module.kotlin.kotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 
@@ -28,6 +30,7 @@ class SocketIOConfig(
             port = socketProperties.port
             origin = "*"
             exceptionListener = socketErrorController
+            jsonSupport = JacksonJsonSupport(kotlinModule())
         }
 
         return SocketIOServer(customConfig)
