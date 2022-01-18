609a579dbdb7f94eb3b33ef76ff80c35dc3f12fc
jhhong0509 <jhhong0509@naver.com>
2022-01-18 18:05:22 +0900

    REFACTORING: Exception 생성자 private으로 변경

--- src/main/kotlin/com/eathub/eathub/global/exception/exceptions/InternalServerError.kt
+++ src/main/kotlin/com/eathub/eathub/global/exception/exceptions/InternalServerError.kt
@@ -3,7 +3,7 @@ package com.eathub.eathub.global.exception.exceptions
 import com.eathub.eathub.global.exception.GlobalException
 import com.eathub.eathub.global.exception.property.GlobalExceptionErrorCode
 
-class InternalServerError : GlobalException(GlobalExceptionErrorCode.UNEXPECTED) {
+class InternalServerError private constructor() : GlobalException(GlobalExceptionErrorCode.UNEXPECTED) {
     companion object {
         @JvmField
         val EXCEPTION = InternalServerError()
