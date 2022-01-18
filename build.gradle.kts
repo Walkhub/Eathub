ff2562cfc4dc4ee0cc524037eb89ad96ae8b6a4e
jhhong0509 <jhhong0509@naver.com>
2022-01-18 16:05:32 +0900

    SETTING: socketio 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,6 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -33,6 +32,9 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+
+    // socketio
+    implementation("com.corundumstudio.socketio:netty-socketio:1.7.19")
 }
 
 tasks.withType<KotlinCompile> {
