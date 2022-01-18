a8a0728cf8cb5fea623cf0458f9f7b8189982fe6
jhhong0509 <jhhong0509@naver.com>
2022-01-18 20:22:09 +0900

    FIX: FoodApplication 작성일 / 복합키 오류 수정

--- src/main/kotlin/com/eathub/eathub/domain/food/application/domain/FoodApplication.kt
+++ src/main/kotlin/com/eathub/eathub/domain/food/application/domain/FoodApplication.kt
@@ -3,21 +3,18 @@ package com.eathub.eathub.domain.food.application.domain
 import com.eathub.eathub.domain.food.application.domain.enums.ApplicationType
 import com.eathub.eathub.domain.food.domain.Food
 import com.eathub.eathub.domain.user.domain.User
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDate
 import javax.persistence.*
 import javax.validation.constraints.NotNull
 
+@EntityListeners(AuditingEntityListener::class)
 @Entity
 class FoodApplication(
-    @EmbeddedId
-    val foodApplicationId: FoodApplicationId,
-
     @Enumerated(EnumType.STRING)
-    @NotNull
+    @Column(nullable = false)
     val applicationType: ApplicationType,
-
-    @NotNull
-    val applicationDate: LocalDate,
 
     @MapsId("foodId")
     @ManyToOne(fetch = FetchType.LAZY)
@@ -26,4 +23,11 @@ class FoodApplication(
     @MapsId("userId")
     @ManyToOne(fetch = FetchType.LAZY)
     val user: User
-)
+) {
+    @CreatedDate
+    @NotNull
+    lateinit var applicationDate: LocalDate
+
+    @EmbeddedId
+    val foodApplicationId: FoodApplicationId = FoodApplicationId(food.id, user.id)
+}
