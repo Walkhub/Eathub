e0bf7b01eadcdb6c9c7f8aa400828c737d63382c
jhhong0509 <jhhong0509@naver.com>
2022-01-18 16:50:31 +0900

    SETTING: SocketErrorController 추가

---
+++ src/main/kotlin/com/eathub/eathub/global/socket/error/SocketErrorController.kt
@@ -0,0 +1,36 @@
+package com.eathub.eathub.global.socket.error
+
+import com.corundumstudio.socketio.SocketIOClient
+import com.corundumstudio.socketio.listener.ExceptionListener
+import com.eathub.eathub.global.exception.GlobalException
+import com.eathub.eathub.global.exception.exceptions.InternalServerError
+import com.eathub.eathub.global.payload.BaseResponse
+import com.eathub.eathub.global.socket.property.EventProperties
+import com.eathub.eathub.global.socket.property.SocketProperties
+import io.netty.channel.ChannelHandlerContext
+import org.springframework.stereotype.Controller
+
+@Controller
+class SocketErrorController : ExceptionListener {
+    override fun onEventException(e: Exception, args: MutableList<Any>?, client: SocketIOClient) = doError(e, client)
+
+    override fun onDisconnectException(e: Exception, client: SocketIOClient) = doError(e, client)
+
+    override fun onConnectException(e: Exception, client: SocketIOClient) = doError(e, client)
+
+    override fun onPingException(e: Exception, client: SocketIOClient) = doError(e, client)
+
+    override fun exceptionCaught(ctx: ChannelHandlerContext, e: Throwable?): Boolean = false
+
+    private fun doError(throwable: Throwable, client: SocketIOClient) {
+        when (throwable) {
+            is GlobalException -> sendErrorMessage(throwable, client)
+            else -> sendErrorMessage(InternalServerError.EXCEPTION, client)
+        }
+    }
+
+    private fun sendErrorMessage(e: GlobalException, client: SocketIOClient) {
+        val errorResponse = BaseResponse.of(e)
+        client.sendEvent(SocketProperties.ERROR, errorResponse)
+    }
+}
