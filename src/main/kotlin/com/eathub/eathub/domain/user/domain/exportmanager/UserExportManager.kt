fe85c97dd8ac6c27e07d86369a9846290679a56e
jhhong0509 <jhhong0509@naver.com>
2022-01-18 19:52:31 +0900

    ADD: UserExportManager 추가

---
+++ src/main/kotlin/com/eathub/eathub/domain/user/domain/exportmanager/UserExportManager.kt
@@ -0,0 +1,21 @@
+package com.eathub.eathub.domain.user.domain.exportmanager
+
+import com.eathub.eathub.domain.user.domain.User
+import com.eathub.eathub.domain.user.domain.repositories.UserRepository
+import com.eathub.eathub.domain.user.exceptions.UserIdNotFoundException
+import com.eathub.eathub.domain.user.exceptions.UserNameNotFoundException
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+
+@Component
+class UserExportManager(
+    private val userRepository: UserRepository
+) {
+    fun findUserByName(name: String): User {
+        return userRepository.findUserByName(name) ?: throw UserNameNotFoundException.EXCEPTION
+    }
+
+    fun findUserById(id: Long): User {
+        return userRepository.findByIdOrNull(id) ?: throw UserIdNotFoundException.EXCEPTION
+    }
+}
