4c2707353e2d9f1bfb9e5eed7b1f236eb4fe16df
jhhong0509 <jhhong0509@naver.com>
2022-01-18 16:05:01 +0900

    SETTING: ConfigurationPropertiesScan 추가

--- src/main/kotlin/com/eathub/eathub/EathubApplication.kt
+++ src/main/kotlin/com/eathub/eathub/EathubApplication.kt
@@ -1,8 +1,12 @@
 package com.eathub.eathub
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+const val BASE_PACKAGE = "com.eathub.eathub"
+
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class EathubApplication
 
