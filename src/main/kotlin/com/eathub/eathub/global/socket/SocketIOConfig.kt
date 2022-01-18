d1d0360def6054663e348dd83b6504607258d8fc
jhhong0509 <jhhong0509@naver.com>
2022-01-18 16:51:12 +0900

    SETTING: SocketIO 설정 추가

---
+++ src/main/kotlin/com/eathub/eathub/global/socket/SocketIOConfig.kt
@@ -0,0 +1,40 @@
+package com.eathub.eathub.global.socket
+
+import com.corundumstudio.socketio.Configuration
+import com.corundumstudio.socketio.SocketConfig
+import com.corundumstudio.socketio.SocketIOServer
+import com.corundumstudio.socketio.annotation.SpringAnnotationScanner
+import com.eathub.eathub.global.socket.error.SocketErrorController
+import com.eathub.eathub.global.socket.property.SocketProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
+
+@Component
+class SocketIOConfig(
+    private val socketErrorController: SocketErrorController,
+    private val socketProperties: SocketProperties
+) {
+    @Bean
+    fun socketIOServer(): SocketIOServer {
+        val customSocketConfig = SocketConfig()
+        val customConfig = Configuration()
+
+        customSocketConfig.apply {
+            isReuseAddress = true
+        }
+
+        customConfig.apply {
+            socketConfig = customSocketConfig
+            port = socketProperties.port
+            origin = "*"
+            exceptionListener = socketErrorController
+        }
+
+        return SocketIOServer(customConfig)
+    }
+
+    @Bean
+    fun springAnnotationScanner(socketServer: SocketIOServer): SpringAnnotationScanner {
+        return SpringAnnotationScanner(socketServer)
+    }
+}
---
+++ src/main/kotlin/com/eathub/eathub/global/socket/SocketRunner.kt
@@ -0,0 +1,16 @@
+package com.eathub.eathub.global.socket
+
+import com.corundumstudio.socketio.SocketIOServer
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
+
+@Component
+@Profile("!test")
+class SocketRunner(
+    private val socketIOServer: SocketIOServer
+) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        socketIOServer.start()
+    }
+}
