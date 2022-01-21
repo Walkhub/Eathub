edd8539fc9412be43ab409674538d9964020b731
jhhong0509 <jhhong0509@naver.com>
2022-01-21 11:45:20 +0900

    UPDATE: 음식 조회 서비스 추가

--- src/main/kotlin/com/eathub/eathub/domain/food/service/FoodService.kt
+++ src/main/kotlin/com/eathub/eathub/domain/food/service/FoodService.kt
@@ -1,10 +1,13 @@
 package com.eathub.eathub.domain.food.service
 
+import com.corundumstudio.socketio.SocketIOClient
 import com.corundumstudio.socketio.SocketIOServer
 import com.eathub.eathub.domain.food.domain.Food
 import com.eathub.eathub.domain.food.domain.repositories.FoodRepository
 import com.eathub.eathub.domain.food.presentation.dto.CreateFoodMessage
 import com.eathub.eathub.domain.food.presentation.dto.CreateFoodRequest
+import com.eathub.eathub.domain.food.presentation.dto.FoodResponse
+import com.eathub.eathub.domain.rate.domain.Rate
 import com.eathub.eathub.domain.restaurant.domain.exportmanager.RestaurantExportManager
 import org.springframework.stereotype.Service
 
@@ -16,6 +19,7 @@ class FoodService(
 ) {
     companion object {
         private const val CREATE_FOOD_KEY = "food.create"
+        private const val FOOD_LIST_KEY = "food.list"
     }
 
     fun createNewFood(request: CreateFoodRequest) {
@@ -41,6 +45,36 @@ class FoodService(
 
         socketIOServer.broadcastOperations
             .sendEvent(CREATE_FOOD_KEY, message)
+    }
+
+    fun getFoodList(socketIOClient: SocketIOClient) {
+        val foods = foodRepository.findAllBy()
+        val foodResponse = foods.map { buildFoodListResponseFromFoodEntity(it) }
+
+        socketIOClient.sendEvent(FOOD_LIST_KEY, foodResponse)
+    }
+
+    private fun buildFoodListResponseFromFoodEntity(food: Food): FoodResponse {
+        return FoodResponse(
+            foodName = food.name,
+            foodCost = food.cost,
+            foodPicture = food.picture,
+            foodScore = getRateAverage(food.rate),
+            restaurantName = food.restaurant.name
+        )
+    }
+
+    private fun getRateAverage(rates: List<Rate>): Double? {
+        val isRateExists = rates.isNotEmpty()
+        val doubleAverage = rates.map { it.score }
+            .average()
+
+        val roundedAverage = String.format("%.2f", doubleAverage).toDouble()
+
+        return when (isRateExists) {
+            true -> roundedAverage
+            false -> null
+        }
     }
 
 }
