77c9a80247ff342d43ab4182830e03f719d0a06e
jhhong0509 <jhhong0509@naver.com>
2022-01-21 11:45:01 +0900

    UPDATE: Food 전체조회 fetch join으로 추가

--- src/main/kotlin/com/eathub/eathub/domain/food/domain/repositories/FoodRepository.kt
+++ src/main/kotlin/com/eathub/eathub/domain/food/domain/repositories/FoodRepository.kt
@@ -1,7 +1,10 @@
 package com.eathub.eathub.domain.food.domain.repositories
 
 import com.eathub.eathub.domain.food.domain.Food
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 
 interface FoodRepository : CrudRepository<Food, Long> {
+    @Query("select f from Food f join fetch f.restaurant left join fetch f.rate")
+    fun findAllBy(): List<Food>
 }
