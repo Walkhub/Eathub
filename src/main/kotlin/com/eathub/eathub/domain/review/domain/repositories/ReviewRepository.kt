556810df2dbb08ab560a7eda60d1860f9f0e987c
jhhong0509 <jhhong0509@naver.com>
2022-01-24 14:44:56 +0900

    UPDATE: Repository에 리뷰 목록 조회 추가

--- src/main/kotlin/com/eathub/eathub/domain/review/domain/repositories/ReviewRepository.kt
+++ src/main/kotlin/com/eathub/eathub/domain/review/domain/repositories/ReviewRepository.kt
@@ -1,8 +1,15 @@
 package com.eathub.eathub.domain.review.domain.repositories
 
+import com.eathub.eathub.domain.food.domain.Food
 import com.eathub.eathub.domain.review.domain.Review
 import com.eathub.eathub.domain.review.domain.ReviewId
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 
 interface ReviewRepository : CrudRepository<Review, ReviewId> {
+
+    @Query("select r from Review r join fetch r.food join fetch r.user where r.food = :food")
+    fun findAllReviewsByFoodId(@Param("food") food: Food): List<Review>
+
 }
