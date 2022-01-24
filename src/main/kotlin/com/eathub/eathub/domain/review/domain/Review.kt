04c6cd35b723e11f7dbfb8bcc2c3bc407ef6abc7
jhhong0509 <jhhong0509@naver.com>
2022-01-24 14:43:24 +0900

    UPDATE: Review엔티티 created_at, content 추가

--- src/main/kotlin/com/eathub/eathub/domain/review/domain/Review.kt
+++ src/main/kotlin/com/eathub/eathub/domain/review/domain/Review.kt
@@ -2,6 +2,8 @@ package com.eathub.eathub.domain.review.domain
 
 import com.eathub.eathub.domain.food.domain.Food
 import com.eathub.eathub.domain.user.domain.User
+import org.springframework.data.annotation.CreatedDate
+import java.time.LocalDateTime
 import javax.persistence.*
 import javax.validation.constraints.NotNull
 
@@ -16,8 +18,15 @@ class Review(
 
     @MapsId("foodId")
     @ManyToOne(fetch = FetchType.LAZY)
-    val food: Food
+    val food: Food,
+
+    val content: String
 ) {
     @EmbeddedId
     val reviewId: ReviewId = ReviewId(user.id, food.id)
+
+    @CreatedDate
+    @NotNull
+    var createAt: LocalDateTime? = null
+        protected set
 }
