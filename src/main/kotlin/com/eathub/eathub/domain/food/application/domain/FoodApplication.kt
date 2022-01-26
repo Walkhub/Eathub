ee077a4413f43872b966f65c46a2c2d6780bf25e
jhhong0509 <jhhong0509@naver.com>
2022-01-27 03:38:12 +0900

    ⚡️ :: FoodApplication 엔티티 수정

--- src/main/kotlin/com/eathub/eathub/domain/food/application/domain/FoodApplication.kt
+++ src/main/kotlin/com/eathub/eathub/domain/food/application/domain/FoodApplication.kt
@@ -1,13 +1,8 @@
 package com.eathub.eathub.domain.food.application.domain
 
 import com.eathub.eathub.domain.food.domain.Food
-import com.eathub.eathub.domain.user.domain.User
-import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.jpa.domain.support.AuditingEntityListener
-import java.time.LocalDate
-import java.time.LocalDateTime
+import com.eathub.eathub.domain.user.domain.ApplicationUser
 import javax.persistence.*
-import javax.validation.constraints.NotNull
 
 @Entity
 class FoodApplication(
@@ -18,15 +13,16 @@ class FoodApplication(
     val food: Food,
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    val user: User,
+    @JoinColumns(
+        JoinColumn(name = "user_id"),
+        JoinColumn(name = "application_type")
+    )
+    val applicationUser: ApplicationUser,
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "foodApplication", cascade = [CascadeType.PERSIST])
     val optionApplication: MutableList<OptionApplication> = mutableListOf()
-) {
-    @NotNull
-    val applicationDate: LocalDateTime = LocalDateTime.now()
 
+) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0
--- src/main/kotlin/com/eathub/eathub/domain/food/application/domain/repositories/FoodApplicationRepository.kt
+++ src/main/kotlin/com/eathub/eathub/domain/food/application/domain/repositories/FoodApplicationRepository.kt
@@ -2,23 +2,18 @@ package com.eathub.eathub.domain.food.application.domain.repositories
 
 import com.eathub.eathub.domain.food.application.domain.FoodApplication
 import com.eathub.eathub.domain.food.application.domain.FoodApplicationId
-import com.eathub.eathub.domain.user.domain.User
+import com.eathub.eathub.domain.user.domain.enums.ApplicationType
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
-import java.time.LocalDateTime
 
 interface FoodApplicationRepository : JpaRepository<FoodApplication, FoodApplicationId> {
-    @Query("select a from FoodApplication a join fetch a.food f join fetch f.restaurant left join fetch a.optionApplication oa left join fetch oa.option where (oa.option.id is null or a.food = oa.option.food) and a.applicationDate between :startDate and :endDate")
-    fun findAllByApplicationDateBetween(
-        @Param("startDate") startDate: LocalDateTime,
-        @Param("endDate") endDate: LocalDateTime
-    ): List<FoodApplication>
+    @Query("select a from FoodApplication a join fetch a.food f join fetch f.restaurant left join fetch a.optionApplication oa left join fetch oa.option where (oa.option.id is null or a.food = oa.option.food) and a.applicationUser.applicationType = :applicationType")
+    fun findAllByApplicationDateBetween(@Param("applicationType") applicationType: ApplicationType): List<FoodApplication>
 
-    @Query("select a from FoodApplication a join fetch a.food f join fetch f.restaurant left join fetch a.optionApplication oa left join fetch oa.option where (oa.option.id is null or a.food = oa.option.food) and a.user.name = :userName and a.applicationDate between :startDate and :endDate")
+    @Query("select a from FoodApplication a join fetch a.food f join fetch f.restaurant left join fetch a.optionApplication oa left join fetch oa.option where (oa.option.id is null or a.food = oa.option.food) and a.applicationUser.user.name = :userName and a.applicationUser.applicationType = :applicationType")
     fun findAllByApplicationDateBetweenAndUserName(
-        @Param("startDate") startDate: LocalDateTime,
-        @Param("endDate") endDate: LocalDateTime,
+        @Param("applicationType") applicationType: ApplicationType,
         @Param("userName") userName: String
     ): List<FoodApplication>
 }
