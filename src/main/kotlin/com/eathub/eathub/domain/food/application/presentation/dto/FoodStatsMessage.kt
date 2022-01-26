02717743a353f29d541e6155814a427291e2e335
jhhong0509 <jhhong0509@naver.com>
2022-01-27 08:39:39 +0900

    ⚡️ :: 돈 상태 메세지 추가

--- src/main/kotlin/com/eathub/eathub/domain/food/application/presentation/dto/FoodStatsMessage.kt
+++ src/main/kotlin/com/eathub/eathub/domain/food/application/presentation/dto/FoodStatsMessage.kt
@@ -1,13 +1,7 @@
-//package com.eathub.eathub.domain.food.application.presentation.dto
-//
-//class FoodStatsMessage(
-//    val totalAmount: Long,
-//    val amountPerPeople: Int,
-//    val remainedAmount: Int,
-//    val menus: FoodStatsMenuMessage
-//)
-//
-//class FoodStatsMenuMessage(
-//    val name: String,
-//    val imageUrl: String,
-//)
+package com.eathub.eathub.domain.food.application.presentation.dto
+
+class FoodStatsMessage(
+    val usedAmount: Long,
+    val amountPerPerson: Int,
+    val remainedAmount: Long
+)
