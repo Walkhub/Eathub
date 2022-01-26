249c496e2a2105df1733e010f43b197b3accc69f
jhhong0509 <jhhong0509@naver.com>
2022-01-27 02:28:58 +0900

    ⚡️ :: FoodApplicationController 추가

--- src/main/kotlin/com/eathub/eathub/domain/food/application/presentation/FoodApplicationController.kt
+++ src/main/kotlin/com/eathub/eathub/domain/food/application/presentation/FoodApplicationController.kt
@@ -1,7 +1,9 @@
 package com.eathub.eathub.domain.food.application.presentation
 
+import com.corundumstudio.socketio.SocketIOClient
 import com.corundumstudio.socketio.annotation.OnEvent
 import com.eathub.eathub.domain.food.application.presentation.dto.FoodApplicationRequest
+import com.eathub.eathub.domain.food.application.presentation.dto.GetFoodApplicationListRequest
 import com.eathub.eathub.domain.food.application.service.FoodApplicationService
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,5 +14,10 @@ class FoodApplicationController(
     @OnEvent("/food/application")
     fun foodApplication(request: FoodApplicationRequest) {
         foodApplicationService.createFoodApplication(request)
+    }
+
+    @OnEvent("/food/application/list")
+    fun foodApplication(socketIOClient: SocketIOClient, request: GetFoodApplicationListRequest) {
+        foodApplicationService.getApplicationList(socketIOClient, request)
     }
 }
