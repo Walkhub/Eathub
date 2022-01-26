c0cdcaaba1d587e28b970d55ac7ce8bb65bc1298
jhhong0509 <jhhong0509@naver.com>
2022-01-27 01:27:59 +0900

    ⚡️ :: FoodApplicationType 삭제, 신청일 추가

--- src/main/kotlin/com/eathub/eathub/domain/food/application/domain/FoodApplication.kt
+++ src/main/kotlin/com/eathub/eathub/domain/food/application/domain/FoodApplication.kt
@@ -1,21 +1,16 @@
 package com.eathub.eathub.domain.food.application.domain
 
-import com.eathub.eathub.domain.food.application.domain.enums.ApplicationType
 import com.eathub.eathub.domain.food.domain.Food
 import com.eathub.eathub.domain.user.domain.User
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDate
+import java.time.LocalDateTime
 import javax.persistence.*
 import javax.validation.constraints.NotNull
 
-@EntityListeners(AuditingEntityListener::class)
 @Entity
 class FoodApplication(
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    val applicationType: ApplicationType,
-
     val count: Int,
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -29,9 +24,8 @@ class FoodApplication(
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "foodApplication", cascade = [CascadeType.PERSIST])
     val optionApplication: MutableList<OptionApplication> = mutableListOf()
 ) {
-    @CreatedDate
     @NotNull
-    lateinit var applicationDate: LocalDate
+    val applicationDate: LocalDateTime = LocalDateTime.now()
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
