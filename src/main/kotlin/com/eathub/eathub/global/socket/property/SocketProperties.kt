dd3cbac6d65a7b4fa4da4ff90dd4a593c0f0ed45
jhhong0509 <jhhong0509@naver.com>
2022-01-26 10:40:06 +0900

    ⚡️ :: SocketProperties에 Option생성을 위한 Property 추가

--- src/main/kotlin/com/eathub/eathub/global/socket/property/SocketProperties.kt
+++ src/main/kotlin/com/eathub/eathub/global/socket/property/SocketProperties.kt
@@ -10,13 +10,16 @@ class SocketProperties(
 ) {
     companion object {
         const val ERROR: String = "error"
-        const val FOOD_INFO_ROOM_KEY = "food.room."
+        private const val FOOD_INFO_ROOM_KEY = "food.room."
+        private const val OPTION_ROOM_KEY = "option.room."
+        const val CREATE_OPTION_KEY = "option.create"
         const val CREATE_FOOD_KEY = "food.create"
         const val FOOD_LIST_KEY = "food.list"
         const val FOOD_INFO_KEY = "food.information"
         const val CREATE_REVIEW_KEY = "review.create"
         const val GET_REVIEW_KEY = "review.list"
         const val FOOD_APPLICATION_KEY = "food.application"
-        fun getFoodRoomName(foodId: Long) = "$SocketProperties.FOOD_INFO_ROOM_KEY$foodId"
+        fun getFoodRoomName(foodId: Long) = "$FOOD_INFO_ROOM_KEY$foodId"
+        fun getOptionRoomName(optionId: Long) = "$OPTION_ROOM_KEY$optionId"
     }
 }
