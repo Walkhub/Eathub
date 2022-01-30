cee3040a8ec6664a2e82388ec04dae25917b2968
jhhong0509 <jhhong0509@naver.com>
2022-01-30 19:43:29 +0900

    ⚡️ :: UserExpoetManager에 username 가져오는거 추가

--- src/main/kotlin/com/eathub/eathub/domain/user/domain/exportmanager/UserExportManager.kt
+++ src/main/kotlin/com/eathub/eathub/domain/user/domain/exportmanager/UserExportManager.kt
@@ -1,8 +1,10 @@
 package com.eathub.eathub.domain.user.domain.exportmanager
 
+import com.corundumstudio.socketio.SocketIOClient
 import com.eathub.eathub.domain.user.domain.User
 import com.eathub.eathub.domain.user.domain.repositories.UserRepository
 import com.eathub.eathub.domain.user.exceptions.UserNameNotFoundException
+import com.eathub.eathub.global.socket.controller.ConnectController
 import org.springframework.stereotype.Component
 
 @Component
@@ -11,5 +13,8 @@ class UserExportManager(
 ) {
     fun findUserByName(name: String): User =
         userRepository.findByName(name) ?: throw UserNameNotFoundException.EXCEPTION
+
+    fun getUserNameFromSocketIOClient(socketIOClient: SocketIOClient): String =
+        socketIOClient.get(ConnectController.CLIENT_NAME_KEY)
 
 }
